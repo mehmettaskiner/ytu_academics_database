26faf87af5328db49704fa08dbb58156415fdeb6
mehmet taskiner <knucles16@gmail.com>
2014-09-24 04:43:44 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+ytu academics database
+=========================
+
+Gives json output of all academics of yildiz techinal university.
+
+educational purposes only.
+
+all data is parsing from http://yarbis1.yildiz.edu.tr
---
+++ fetch_academics.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import urllib, urllib2
+import json
+import os
+from bs4 import BeautifulSoup
+
+class Academic_Fetcher():
+    def __init__(self):
+        self.url = 'http://www.yarbis1.yildiz.edu.tr/face/ara/loadSearchResults.php'
+        self.academics = []
+
+    def start_fetching(self):
+        alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                    'O', 'P', 'R', 'S', 'T', 'U', 'V', 'Y', 'Z']
+        for letter in alphabet:
+            # post request for every letter
+            url_2 = 'http://www.yarbis1.yildiz.edu.tr/face/ara/loadSearchResults.php'
+            values = dict(harf=letter)
+            data = urllib.urlencode(values)
+            req = urllib2.Request(url_2, data)
+            rsp = urllib2.urlopen(req)
+            content = rsp.read()
+            self.process_soup(self.turn_data_to_soup(content))
+            print 'academics starts with ' + letter + ' completed.'
+
+    def turn_data_to_soup(self, data):
+        soup = BeautifulSoup(data)
+        return soup
+
+    def process_soup(self, soup):
+        all_rows = soup.find_all("tr", attrs = {'style': 'background-color:#ddd;'})
+        for row in all_rows:
+            academic_name = row.find("td", attrs = {'width': '120px'}).text
+            academic_department = row.find("td", attrs = {'width': '150px'}).text
+            academic_title = row.find("td", attrs = {'width': '60px'}).text
+            academic_homepage_row = row.find("td", attrs = {'width': '70px'})
+            academic_homepage = academic_homepage_row.find('a')['href']
+            academic = {
+                'name': academic_name,
+                'department': academic_department,
+                'title': academic_title,
+                'homepage': academic_homepage
+            }
+            self.academics.append(academic)
+
+    def jsonify(self):
+        return json.dumps(self.academics, indent=4, separators=(',', ': '))
+
+    def write_json(self):
+        dir = os.path.dirname(os.path.abspath(__file__))
+        filename = "academics.json"
+        file = open(dir + "/" + filename, "wb")
+        file.write(self.jsonify())
+        file.close()
+
+
+if __name__ == '__main__':
+    academic_fetcher = Academic_Fetcher()
+    academic_fetcher.start_fetching()
+    academic_fetcher.jsonify()
+    academic_fetcher.write_json()
+    print "total %d academics." % (len(academic_fetcher.academics))
